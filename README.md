de747fad6a80a6e24c11138e645ddd188dffc4c5
brichbourg <brichbourg@gmail.com>
2015-04-09 06:44:51 -0400

    first commit

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Brantley Richbourg <brichbourg@gmail.com>
 #Installation and Usage
 
 1.) Clone repo:
-	git clone 
+	git clone https://github.com/brichbourg/PingList.git
 
 2.) Edit ips.txt to populate with your list of random IP addresses you want to test.
 
@@ -23,4 +23,4 @@ Brantley Richbourg <brichbourg@gmail.com>
 
 This script uses a variation of the Python Ping script (http://www.g-loaded.eu/2009/10/30/python-ping/).  This code is included in this repo and is imported by PingList.py.  The use of this script requires running PingList.py as root, hence the reason to run with sudo.
 
-This script was created on MAC OS 10.10.2.  No other operating systems were tested.# PingList
+This script was created on MAC OS 10.10.2.  No other operating systems were tested.
---
+++ pinglist.py
@@ -0,0 +1,39 @@
+# Author: Brantley Richbourg <brichbourg AT gmail.com>
+#Used Python Pure Ping -  http://www.g-loaded.eu/2009/10/30/python-ping/
+#Python Ping imported at 'pure ping'
+
+import os
+import sys
+import pureping
+
+args = sys.argv
+
+if len(args) == 2:
+
+
+	if os.path.exists(args[1]):
+		switch_ips = open(args[1]).read().splitlines()
+		print '\nIP addresses are ', switch_ips
+		print "=" * 25
+		print "START PING TEST"
+		print "=" * 25
+
+		for each in switch_ips:
+			testip = pureping.do_one(each,1)
+			if testip == None:
+				print each, "is dead."
+			else:
+				print '*', each, "IS ALIVE!!!"
+						
+		print "=" * 25
+		print "END PING TEST"
+		print "=" * 25
+
+	else:
+		print 'File, ', os.path.realpath(args[1]), 'does not exist.  Please try again.'
+
+else:
+	print 'ERROR: Invalid syntax\n'\
+	'Example: \"python ping-addresses.py file.txt\"'
+	sys.exit(1)
+
